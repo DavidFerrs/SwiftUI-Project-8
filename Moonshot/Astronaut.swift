8b509338bd821616863e7ff70921190b6f9bae6e
DavidFerrs <fernandes.-.david@hotmail.com>
2025-03-01 14:49:24 -0300

    Showing mission details with scrollview and containerRelativeFrame, loading data with codable json data

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by David Ferreira on 27/02/25.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
